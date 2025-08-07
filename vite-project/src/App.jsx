b6a23438a5c19a5e7a6a92700575a338514397d8
Pothe <keoutpothe2014@gmail.com>
2025-08-07 21:37:14 +0700

    app

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import Demo from './Demo'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './pages/About'
+import Contact from './components/Contact'
+import Navigation from './components/Navigation'
 import Notfound from './Notfound'
+import Home from './pages/Home'
+
+
 
 function App() {
   return (
     <div>
     
      <BrowserRouter>
+     <Navigation/>
      <Routes>
-      <Route path='/' element={<Demo/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
 
-      <Route path='*' element={<Notfound/>}/>
+      <Route path='/contact' element={<Contact/>}/>
         <Route path='*' element={<Notfound/>}/>
      </Routes>
      </BrowserRouter>
