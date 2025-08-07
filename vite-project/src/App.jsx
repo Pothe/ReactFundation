1461da1622cc814c4bf5929ad20fd2f8f6a48dcf
Pothe <keoutpothe2014@gmail.com>
2025-08-07 16:25:45 +0700

    vite react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
-
-
-
+import Demo from './Demo'
 function App() {
+  let mark =60
 
+ 
 
   return (
-    <>
-      <div>
-       Hello to React
-      </div>
+    <div>
+      
+       
+       
+    {mark}
+    { (()=>{
+
+      if(mark>80 && mark<100){
+        return <h1>good</h1>
+      }else if(mark>60 && mark<80){
+        return <h1>fair</h1>
+      }else{
+        return <h1>F</h1>
+      }
+    })()}
     
-    </>
+    </div>
   )
 }
 
---
+++ vite-project/src/Demo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Demo(){
+    return(
+        <>
+    <div>
+        <input type="text"  />
+        <button type="submit">Submit</button>
+    </div>
+       
+        </>
+    )
+}
+export default Demo
