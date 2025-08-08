1fb8ba2ead38fd264f999c9de1804b6f578dcdec
Pothe <keoutpothe2014@gmail.com>
2025-08-08 14:34:42 +0700

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,39 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 function App() {
-  const [list, setlist] = useState([]);
-  const [iterm ,setiterm] = useState('')
-  const AddTodo =()=>{
-   list.push(iterm)
-   setlist([...list])
-   
+  const [list,setlist] =useState([])
+  const [iterm,setiterm] = useState("")
+  const input = useRef()
+  const Addlist =()=>{
+    list.push(iterm)
+    setlist([...list])
+    setiterm('')
+    input.current.focus()
+
+    
   }
   return (
     <div>
-      <span>{list.length}</span>
-     <input type="text" value={iterm} onChange={(e)=>setiterm(e.target.value)} />
-     <button onClick={AddTodo}>Add</button>
-     <table>
+    <div>{list.length}</div>
+    <input ref={input} type="text" value={iterm} onChange={(e)=>setiterm(e.target.value)} />
+    <button onClick={Addlist}>Add List</button>
+    <table>
       <tbody>
         {
-          list.length !==0? list.map((itermlist,i)=>{
-         return(
-            <tr key={i}>
-            <td>{itermlist}{i}</td>
-            <td><button>remove</button></td> 
-           </tr>
-         )
+          list.length!==0? list.map((itermlist,i)=>{
+            return(
+          <tr key={i}>
+            <td>{itermlist}</td>
+            <td><button>Remove</button></td>
+          </tr>
+            )
           }):(null)
 
         }
-      
+        
       </tbody>
-     </table>
+    </table>
+    
     </div>
   )
 }
