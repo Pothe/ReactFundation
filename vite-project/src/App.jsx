73e4a14d41b09277aa9480861b6f74ac66ddb1ef
Pothe <keoutpothe2014@gmail.com>
2025-08-08 16:34:03 +0700

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [FormObject,setFormObject] =useState({Fname:"",Lname:"",city:"",Gender:""})
+
+  const InputOnchage =({property,value})=>{
+    setFormObject(previous=>({
+      ...previous,[property]:value
+    }
+      
+   
+    ))
+     console.log(FormObject.Fname)
+  }
   return (
     <div>
       <form >
-        <input type="text" placeholder='First name' />
-        <input type="text" placeholder='last name' />
-        <select >
+        <input value={FormObject.Fname} type="text" placeholder='First name' onChange={(e)=>{InputOnchage({property:'Fname',value:e.target.value})}}/>
+        <input value={FormObject.Lname} type="text" placeholder='last name' />
+        <select value={FormObject.city} >
           <option value="unknow">Choose City</option>
           <option value="phnom penh">phnom penh</option>
           <option value="shv">Sihanoukville</option>
           <option value="kampot">Kampot</option>
           
         </select>
-           <input type="radio" name="Female" id="" />Male
-        <input type="radio" name="Female" id="" />Female
+           <input checked={FormObject.Gender ==="Male"} type="radio"  />Male
+        <input checked={FormObject.Gender ==="Female"} type="radio" name="Female" id="" />Female
         <br></br>
         <button type="submit">Submit</button>
       </form>
